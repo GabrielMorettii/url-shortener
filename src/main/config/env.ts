ab75d406841d626cd45117829baa33ec43e020bd
GabrielMorettii <gabrielmorettisilva@gmail.com>
2024-11-01 10:03:31 -0300

    feat: add .env.example

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   enviroment: process.env.NODE_ENV ?? "development",
   port: process.env.HTTP_PORT ?? 3030,
-  jwtSecret: process.env.JWT_SECRET ?? "any-secret",
+  jwtSecret: process.env.JWT_SECRET_KEY ?? "any-secret",
 };
