26e3aa909240fe97a288f7780fa3d5072740e624
GabrielMorettii <gabrielmorettisilva@gmail.com>
2024-11-07 11:44:08 -0300

    test: add tests for bcrypt adapter

--- jest.config.ts
+++ jest.config.ts
@@ -91,6 +91,7 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
+    "^@/tests/(.*)$": "<rootDir>/tests/$1",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
 
