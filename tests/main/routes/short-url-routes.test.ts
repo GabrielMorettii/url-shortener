73762ada2a1447021edb9b86349d4ead04a0085f
GabrielMorettii <gabrielmorettisilva@gmail.com>
2024-11-07 09:54:33 -0300

    refactor: alter short url tests to be more precise

--- tests/main/routes/short-url-routes.test.ts
+++ tests/main/routes/short-url-routes.test.ts
@@ -3,7 +3,7 @@ import { type Express } from "express";
 
 import { prisma } from "@/infra/db";
 import { setupApp } from "@/main/config/app";
-import { mockAccessToken, mockShortUrl } from "./mocks";
+import { mockAccessToken, mockShortUrl, mockUser } from "./mocks";
 
 describe("ShortUrl Routes", () => {
   let app: Express;
@@ -24,25 +24,45 @@ describe("ShortUrl Routes", () => {
   });
 
   describe("POST /short-url", () => {
-    it("Should return 201 on create short url (without auth)", async () => {
-      await request(app)
+    it("Should return 201 and save the short URL in the database (without auth)", async () => {
+      const originalUrl = "https://www.google.com/";
+
+      const response = await request(app)
         .post("/short-url")
         .send({
-          url: "https://www.google.com/",
+          url: originalUrl,
         })
         .expect(201);
+
+      const shortUrl = await prisma.shortUrl.findFirst({
+        where: { originalUrl },
+      });
+
+      expect(response.body.shortUrl).not.toBeNull();
+      expect(shortUrl).not.toBeNull();
+      expect(shortUrl?.clicks).toBe(0);
     });
 
-    it("Should return 201 on create short url (authorized)", async () => {
+    it("Should return 201 and save the short URL in the database (authorized)", async () => {
       const accessToken = await mockAccessToken();
+      const originalUrl = "https://www.google.com";
 
-      await request(app)
+      const response = await request(app)
         .post("/short-url")
         .set("Authorization", `Bearer ${accessToken}`)
         .send({
-          url: "https://www.google.com",
+          url: originalUrl,
         })
         .expect(201);
+
+      const shortUrl = await prisma.shortUrl.findFirst({
+        where: { originalUrl },
+      });
+
+      expect(response.body.shortUrl).not.toBeNull();
+      expect(shortUrl).not.toBeNull();
+      expect(shortUrl?.clicks).toBe(0);
+      expect(shortUrl?.userId).not.toBeNull();
     });
   });
 
@@ -50,18 +70,36 @@ describe("ShortUrl Routes", () => {
     it("Should return 302 on get short url(redirected)", async () => {
       const shortUrlModel = await mockShortUrl();
 
-      await request(app).get(`/${shortUrlModel.shortUrl}`).expect(302);
+      const response = await request(app)
+        .get(`/${shortUrlModel.shortUrl}`)
+        .expect(302);
+
+      expect(response.header.location).toBe(shortUrlModel?.originalUrl);
     });
   });
 
   describe("GET /short-url/user", () => {
     it("Should return 200 on get user short urls", async () => {
-      const accessToken = await mockAccessToken();
+      const mockUserModel = await mockUser();
+      const [shortUrlModel, accessToken] = await Promise.all([
+        mockShortUrl(mockUserModel.id),
+        mockAccessToken(mockUserModel.id),
+      ]);
 
-      await request(app)
+      const response = await request(app)
         .get("/short-url/user")
         .set("Authorization", `Bearer ${accessToken}`)
         .expect(200);
+
+      const expectedResponse = [
+        {
+          ...shortUrlModel,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+      ];
+
+      expect(response.body).toMatchObject(expectedResponse);
     });
   });
 });
